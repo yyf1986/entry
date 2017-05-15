64a94a5b82e722244f32501728ec23ac555fa9e4
YAO <yyf1986@users.noreply.github.com>
2017-05-15 15:49:48 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-
+	"flag"
 	"github.com/laincloud/entry/server"
 )
 
 func main() {
-	server.StartServer("8888", "unix:///var/run/docker.sock")
+	//server.StartServer("8888", "unix:///var/run/docker.sock")
+	var port string
+	flag.StringVar(&port,"port","8888","http listen port")
+	flag.Parse()
+	server.StartServer(port,"unix:///var/run/docker.sock")
 }
